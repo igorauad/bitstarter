c1bbdf6faec821ecc31beb84cc8243ee441900ad
Igor Freire <igor.auad@gmail.com>
2013-11-20 14:17:30 +0000

    Carrousel included on index. Launchrock page with new title.

--- web.js
+++ web.js
@@ -2,16 +2,31 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 var url = require("url");
+
+app.configure(function(){
+    app.use('/public', express.static(__dirname + "/public"));
+});
+
+/* Middleware for logging:  */
 app.use(express.logger());
+
+/* Middleware for parsing post body */
+app.use(express.bodyParser());
+
+app.get('/', function(request, response) {
+    var buffer = fs.readFileSync('signup.html');
+    response.send(buffer.toString());
+});
 
 app.get('/index', function(request, response) {
     var buffer = fs.readFileSync('index.html');
     response.send(buffer.toString());
 });
  
-app.get('/', function(request, response) {
-    var buffer = fs.readFileSync('signup.html');
-    response.send(buffer.toString());
+app.post('/preRegister', function(req, res) {
+    var name = req.body.inputName;
+    var buffer = fs.readFileSync('index.html');
+    res.send(buffer.toString());
 });
 
 var port = process.env.PORT || 8080;
